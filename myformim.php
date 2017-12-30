b1ba8cb6758a8336a292a0fffd775b5277e6b323
Peter Myrvig <myrvig@gmail.com>
2017-12-30 15:01:29 +0100

    no message

--- myformim.php
+++ myformim.php
@@ -7,38 +7,6 @@
         <meta charset="UTF-8">
         <title><?php echo $title;?></title>
         <link rel="stylesheet" href="./css/ifdemo.css"/>
-        <script>
-            var $ = function (nml) {
-                return document.getElementById(nml);
-            }
-            
-            var dispSlide = function () {
-                $('ranger').innerHTML = $('dimples').value;
-            }
-            
-            var isValid = function (e) {
-                if (e.target.color.value === '') {
-                    window.alert('Missing color');
-                    e.target.color.focus()
-                    e.preventDefault();
-                    return false;                 
-                }
-                if (e.target.img.value === '') {
-                    window.alert('Missing image');
-                    e.target.img.focus()
-                    e.preventDefault();
-                    return false;                 
-                }
-                return true;
-            }
-            
-            var init = function () {
-                $('deform').addEventListener('submit', isValid);
-                dispSlide();
-                $('dimples').addEventListener('mousemove', dispSlide);
-            }
-            window.addEventListener('load', init);
-        </script>
     </head>
     <body>
         <?php
@@ -50,15 +18,6 @@
               enctype="multipart/form-data">
             <fieldset>
                 <legend>Golfballs</legend>
-            <p>
-                <label for='color'>Color:</label><br/>
-                <input type='text' id='color' name='color'/>
-            </p>
-            <p>
-                <label for='dimples'>No of Dimples:</label><br/>
-                <input type="range" id="dimples" name="dimples"  
-                       min="350" max="500" value="375" required/> <span id='ranger'></span>
-            </p>
             <p>
                 <input type="hidden" name="MAX_FILE_SIZE" value="131072"/>
                 <label for='bild'>Image:</label><br/>
